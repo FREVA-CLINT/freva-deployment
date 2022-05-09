b7164966bffcb2722be4fda534a37517534c3102
k204230 <bergemann@dkrz.de>
2022-05-09 07:35:17 +0200

    Lint with black

--- docs/conf.py
+++ docs/conf.py
@@ -13,18 +13,19 @@
 import os
 import sys
 from recommonmark.parser import CommonMarkParser
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../src'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'freva-deployment'
-copyright = '2022, Martin Bergemann'
-author = 'Martin Bergemann'
+project = "freva-deployment"
+copyright = "2022, Martin Bergemann"
+author = "Martin Bergemann"
 
 # The full version, including alpha/beta/rc tags
-release = '2022.03.01'
+release = "2022.03.01"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,22 +34,22 @@ release = '2022.03.01'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc',
-        'sphinx.ext.intersphinx',
-        'sphinx.ext.napoleon',
-        'nbsphinx',
-        'recommonmark',
-        'sphinx.ext.viewcode',
-        'sphinxcontrib_github_alt',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "recommonmark",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib_github_alt",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -56,15 +57,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 source_parsers = {
-    '.md': CommonMarkParser,
+    ".md": CommonMarkParser,
 }
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
