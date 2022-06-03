21d0c9ace663eed7c3a99d5bed378a815e79fa3f
k204230 <bergemann@dkrz.de>
2022-06-03 09:23:03 +0200

    zip mysql backup file straight away

--- assets/db_service/daily_backup.sh
+++ assets/db_service/daily_backup.sh
@@ -2,9 +2,7 @@
 ###############################################
 # CREATE a daily backup of the freva database
 mkdir -p ${BACKUP_DIR}
-backup_f=${BACKUP_DIR}/backup-$(date +%Y%m%d_%H%M%S).sql
-mysqldump -u root -h localhost -p"${MYSQL_ROOT_PASSWORD}" --all-databases > $backup_f
-gzip -9 -q $backup_f
+backup_f=${BACKUP_DIR}/backup-$(date +%Y%m%d_%H%M%S).sql.gz
 files_to_keep=$(ls -t ${BACKUP_DIR}/backup-*.sql.gz |head -n ${NUM_BACKUPS})
 for file in $(ls ${BACKUP_DIR}/backup-*.sql.gz);do
     is_new_file=$(echo ${files_to_keep} |grep $file)
@@ -12,4 +10,5 @@ for file in $(ls ${BACKUP_DIR}/backup-*.sql.gz);do
         rm $file
     fi
 done
-chown mysql:mysql ${backup_f}.gz
+mysqldump -u root -h localhost -p"${MYSQL_ROOT_PASSWORD}" --all-databases | gzip -c -9 -q > $backup_f
+chown mysql:mysql ${backup_f}
