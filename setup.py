877c650324750b47b9782e05b5580b092ce59bf3
k204230 <bergemann@dkrz.de>
2022-07-01 10:37:12 +0200

    Add include_metadata flag

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ setup(
     description="Deploy freva and its services on different machines.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
+    include_package_data=True,
     license="GPLv3",
     packages=find_packages("src"),
     package_dir={"": "src"},
