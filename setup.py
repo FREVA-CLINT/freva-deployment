f009bdb654418952dcb846c6f0b9640c26892426
k204230 <bergemann@dkrz.de>
2022-07-01 10:55:13 +0200

    Include package metadata

--- setup.py
+++ setup.py
@@ -101,6 +101,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     include_package_data=True,
+    package_data={"assets": ["assets"]},
     license="GPLv3",
     packages=find_packages("src"),
     package_dir={"": "src"},
