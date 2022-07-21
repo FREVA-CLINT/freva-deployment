afd59845c12616a70eb759bbbe0e20d936b729bb
k204230 <bergemann@dkrz.de>
2022-07-21 11:19:47 +0200

    Improve reset of root pw

--- assets/db_service/reset_root_pw.sh
+++ assets/db_service/reset_root_pw.sh
@@ -2,17 +2,11 @@
 ### Reset the root password
 set -e
 
-pw_file=/var/lib/mysql/.pw_file
 
-if [ ! -f "$pw_file" ];then
-    echo $MYSQL_ROOT_PASSWORD > $pw_file
-fi
-root_pw=$(cat $pw_file)
 echo "USE mysql; FLUSH PRIVILEGES; ALTER USER "\
      "'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; "\
      "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; "\
      "ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; "\
      "FLUSH PRIVILEGES;" > /tmp/my.sql
-mysql -p$root_pw -u root < /tmp/my.sql
-echo $MYSQL_ROOT_PASSWORD > $pw_file
+mysql -u root < /tmp/my.sql
 rm /tmp/my.sql
