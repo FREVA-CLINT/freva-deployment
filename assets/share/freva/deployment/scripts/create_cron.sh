4e1b19458f8263d6d7106a75e87d67e3471d103e
antarcticrainforest <martin.bergemann@posteo.org>
2024-02-18 00:24:40 +0100

    Add if statement for non-root user crontab creation

--- assets/share/freva/deployment/scripts/create_cron.sh
+++ assets/share/freva/deployment/scripts/create_cron.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 cmd="/usr/local/bin/docker-or-podman exec $1 bash /usr/local/bin/daily_backup 1> /dev/null"
-mkdir -p /etc/cron.daily
-echo "#!/bin/sh" > /etc/cron.daily/backup_$1
-echo "# Run daily backup for $1" >> /etc/cron.daily/backup_$1
-if [ "$2" ];then
-    echo "MAILTO=$2"
+# Function to check if the cron job already exists in the crontab
+cron_job_exists() {
+    local cmd="$1"
+    crontab -l 2>/dev/null | grep -F "$cmd" >/dev/null
+}
+if [ -w /etc/cron.daily ]; then
+    # If /etc/cron.daily is writable, create the cron job there
+    cron_dir="/etc/cron.daily"
+    cron_file="$cron_dir/backup_$1"
+else
+    # If /etc/cron.daily is not writable, add the cron job to the user's crontab
+    cron_dir=""
+    cron_file="user"
 fi
-echo $cmd >> /etc/cron.daily/backup_$1
-chmod +x /etc/cron.daily/backup_$1
+
+
+if [ "$cron_dir" ]; then
+    mkdir -p "$cron_dir"
+    echo "#!/bin/sh" > "$cron_file"
+    echo "# Run daily backup for $1" >> "$cron_file"
+    if [ "$2" ]; then
+        echo "MAILTO=$2" >> "$cron_file"
+    fi
+    echo "$cmd" >> "$cron_file"
+    chmod +x "$cron_file"
+else
+     if ! cron_job_exists "$cmd"; then
+        echo "0 0 * * * $cmd" >> "$HOME/.crontab"
+        crontab "$HOME/.crontab"
+    fi
+fi
