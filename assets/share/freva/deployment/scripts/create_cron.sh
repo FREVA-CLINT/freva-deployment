7d5625ffe3114bfbb8eebc00abc70e0d8186cb07
antarcticrainforest <martin.bergemann@posteo.org>
2024-03-13 02:15:52 +0100

    Fix typo

--- assets/share/freva/deployment/scripts/create_cron.sh
+++ assets/share/freva/deployment/scripts/create_cron.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cmd=$(/tmp/docker-or-podman -p exec $1 bash /usr/local/bin/daily_backup)
 cmd="$cmd 1> /dev/null"
-echo $cmd
-exit
 # Function to check if the cron job already exists in the crontab
 cron_job_exists() {
     local cmd="$1"
