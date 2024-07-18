ede666f613c6f6ae360b24e9d7b47d84d324ab4a
antarcticrainforest <martin.bergemann@posteo.org>
2024-07-18 14:29:27 +0200

    Mock ansible encoding if it's not utf-8

--- pyinstaller/pyinstaller-deploy-freva.py
+++ pyinstaller/pyinstaller-deploy-freva.py
@@ -1,7 +1,16 @@
 import os
-from freva_deployment.cli import main_cli
+import re
+import locale
+import sys
 
-if __name__ == "__main__":
+import mock
+from rich import print as pprint
+
+
+def main():
+    """Run the main tui."""
+    from freva_deployment.cli import main_cli
+
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin")
     for exe in ("cowsay", "cowsay.exe"):
         cow_exe = os.path.join(path, exe)
@@ -10,3 +19,17 @@ if __name__ == "__main__":
             break
     os.environ["PATH"] = path + os.pathsep + os.environ["PATH"]
     main_cli()
+
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    dummy, encoding = locale.getlocale()
+    if encoding.lower() not in ("utf-8", "utf8"):
+        pprint(
+            f"[red][b]WARNING[/b] - Your locale encoding is {encoding} "
+            "Ansible might not work correctly. Consider changing your "
+            "locale encoding (LC_ALL) to UTF-8[/red]"
+        )
+    with mock.patch("locale.getlocale", lambda: ("utf-8", "utf-8")):
+        with mock.patch("sys.getfilesystemencoding", lambda: "utf-8"):
+            main()
