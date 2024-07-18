4a4cbd4b491625f6eb55a5f6305e97957d2de53b
antarcticrainforest <martin.bergemann@posteo.org>
2024-07-18 02:50:31 +0200

    Handle different cowsay suffix paths on osx and windows

--- pyinstaller/pyinstaller-deploy-freva.py
+++ pyinstaller/pyinstaller-deploy-freva.py
@@ -3,9 +3,10 @@ from freva_deployment.cli import main_cli
 
 if __name__ == "__main__":
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin")
-    from pathlib import Path
-
-    print(list(Path(path).rglob("*")))
-    os.environ["ANSIBLE_COW_PATH"] = os.path.join(path, "cowsay")
+    for exe in ("cowsay", "cowsay.exe"):
+        cow_exe = os.path.join(path, exe)
+        if os.path.isfile(cow_exe):
+            os.environ["ANSIBLE_COW_PATH"] = os.path.join(exe)
+            break
     os.environ["PATH"] = path + os.pathsep + os.environ["PATH"]
     main_cli()
