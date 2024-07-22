d7c8ad2304fa6a9215928b376189462e822d489f
antarcticrainforest <martin.bergemann@posteo.org>
2024-07-22 14:32:14 +0200

    Mock libc wcwidth.

--- pyinstaller/pre-win.py
+++ pyinstaller/pre-win.py
@@ -13,7 +13,9 @@ if sys.platform.lower().startswith("win"):
     try:
         import ansible
 
-        ansible_cli_path = Path(ansible.__file__).parent / "cli" / "__init__.py"
+        ansible_cli_path = (
+            Path(ansible.__file__).parent / "cli" / "__init__.py"
+        )
     finally:
         sys.getfilesystemencoding = getfilesystemencoding
         locale.getlocale = getlocale
@@ -27,15 +29,24 @@ if sys.platform.lower().startswith("win"):
             if f"os.{call}" in content:
                 write = True
                 if "import os, termios" not in content:
-                    content = content.replace("import os", "import os, termios")
+                    content = content.replace(
+                        "import os", "import os, termios"
+                    )
                 content = content.replace(f"os.{call}", f"termios.{call}")
-        if "get_context('fork')" in content or 'get_context("fork")' in content:
+        if (
+            "get_context('fork')" in content
+            or 'get_context("fork")' in content
+        ):
             write = True
-            content = content.replace("get_context('fork')", "get_context('spawn')")
-            content = content.replace('get_context("fork")', 'get_context("spwan")')
+            content = content.replace(
+                "get_context('fork')", "get_context('spawn')"
+            )
+            content = content.replace(
+                'get_context("fork")', 'get_context("spwan")'
+            )
         if write:
             inp_file.write_text(content, encoding="utf-8")
-    display = Path(ansible.__file__) / "utils" / "display.py"
+    display = Path(ansible.__file__).parent / "utils" / "display.py"
     content = "\n".join(
         [
             l
@@ -43,7 +54,9 @@ if sys.platform.lower().startswith("win"):
             if not l.startswith("_LIBC =")
         ]
     )
-    content = content.replace("import ctypes.util", "ctypes.util\nimport wcwidth")
+    content = content.replace(
+        "import ctypes.util", "ctypes.util\nimport wcwidth"
+    )
     display.write_text(content, encoding="utf-8")
 else:
     import PyInstaller.depend.bindepend
